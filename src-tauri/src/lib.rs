5ad4d70447734ff742105a6ab36456850ef2224b
SkidRow <51795604+RolandoHidalgo@users.noreply.github.com>
2026-05-30 23:55:44 -0400

    - con updater al fin

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -14,6 +14,7 @@ fn greet(name: &str) -> String {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_updater::Builder::new().build())
         .on_page_load(|window, _payload| {
             let args: Vec<String> = std::env::args().collect();
 
